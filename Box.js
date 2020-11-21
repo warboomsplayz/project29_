3ba4f2feef7de9cb9df0b9d1b84ad323e9170897
warboomsplayz <70512932+warboomsplayz@users.noreply.github.com>
2020-11-21 19:43:05 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,7 @@
+class Box extends BaseClass {
+  constructor(x, y){
+    super(x,y,60,60);
+    this.image = loadImage("sprites/wood1.png");
+  }
+
+};
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# project29_
+# p5.play-boilerplate
+Boiler plate for p5.play
